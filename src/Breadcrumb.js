963b108e855af5ba0f92a7383c457345d1aec7da
bonjunku <ku96kbj@naver.com>
2022-02-14 00:33:50 +0900

    FEAT : Breadcrumb 클릭시 이동하도록 설정

--- src/Breadcrumb.js
+++ src/Breadcrumb.js
@@ -1,6 +1,6 @@
-export default function Breadcrumb({ $app, initialState }) {
+export default function Breadcrumb({ $app, initialState, onClick }) {
   this.state = initialState;
-
+  this.onClick = onClick;
   this.$target = document.createElement('nav');
   this.$target.className = 'Breadcrumb';
   $app.appendChild(this.$target);
@@ -18,4 +18,16 @@ export default function Breadcrumb({ $app, initialState }) {
       )
       .join('')}`;
   };
+
+  //이벤트 위임 이용
+  this.$target.addEventListener('click', (e) => {
+    const $navItem = e.target.closest('.nav-item');
+
+    if ($navItem) {
+      const { index } = $navItem.dataset;
+      this.onClick(index ? parseInt(index, 10) : null);
+    }
+  });
+
+  this.render();
 }
